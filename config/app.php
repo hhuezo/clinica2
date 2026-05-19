194a3a267aff327beece74e55186c872cf54b76f
Alexander Huezo <hugo.alex.huezo@gmail.com>
2026-05-19 15:26:24 -0600

    Corrige el nombre de la aplicación a "Gestión Medica SV" en el archivo de configuración y el ejemplo de variables de entorno.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Gestion Medica SV'),
+    'name' => env('APP_NAME', 'Gestión Medica SV'),
 
     /*
     |--------------------------------------------------------------------------
